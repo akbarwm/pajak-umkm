78e409f4ca4a7e76a782292f5757f92382fd5356
daffaqshal <daffaaqshal04@gmail.com>
2023-10-16 12:43:43 +0700

    kategori usaha

--- kategoriusaha.php
+++ kategoriusaha.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    
+
 <head>
     <title>Kategori Perbidang Usaha</title>
     <!-- Favicon icon -->
@@ -35,34 +35,87 @@
         </div>
     </div>
     <!-- Breadcrumbs End -->
-            <!-- Privacy Policy Start -->
-            <div id="neuron-privacy" class="neuron-privacy pt-90 pb-90 md-pt-73 md-pb-75">
-            <div class="container">
-                <div class="privacy-part">
-                    <div class="single-privacy mb-50">
-                        <h2 class="privacy-title semi-bold mb-20">Kategori Usaha</h2>
-                        <p class="privacy-desc margin-0">Kategori usaha mengacu pada pengelompokan bisnis berdasarkan karakteristik umum, industri, atau jenis kegiatan yang mereka lakukan. Hal ini membantu dalam memahami dan mengklasifikasikan berbagai jenis usaha agar lebih mudah dianalisis, diatur, dan dibandingkan.</p>
-                        <p class="privacy-desc margin-0">Kategori usaha umumnya melibatkan parameter seperti jenis produk atau layanan yang ditawarkan, model bisnis, ukuran perusahaan, dan sektor industri.</p>
+    <!-- Privacy Policy Start -->
+    <div id="neuron-privacy" class="neuron-privacy pt-90 pb-90 md-pt-73 md-pb-75">
+        <div class="container">
+            <div class="privacy-part">
+                <div class="single-privacy mb-50">
+                    <h2 class="privacy-title semi-bold mb-20">Kategori Usaha</h2>
+                    <p class="privacy-desc margin-0">Kategori usaha mengacu pada pengelompokan bisnis berdasarkan karakteristik umum, industri, atau jenis kegiatan yang mereka lakukan. Hal ini membantu dalam memahami dan mengklasifikasikan berbagai jenis usaha agar lebih mudah dianalisis, diatur, dan dibandingkan.</p>
+                    <p class="privacy-desc margin-0">Kategori usaha umumnya melibatkan parameter seperti jenis produk atau layanan yang ditawarkan, model bisnis, ukuran perusahaan, dan sektor industri.</p>
+                </div>
+                <div class="single-privacy mb-50">
+                    <h2 class="privacy-title semi-bold mb-20">Jenis Jenis Kategori Usaha</h2>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/kuliner.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bidang Kuliner</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
-                    <div class="single-privacy mb-50">
-                        <h2 class="privacy-title semi-bold mb-20">Jenis Jenis Kategori Usaha</h2>
-                        <h3 class="privacy-list-title semi-bold mb-20"><span>1</span>. Bidang Kuliner </h3>
-                        <p class="privacy-desc">Bidang kuliner merujuk pada sektor industri yang berhubungan dengan produksi, penyajian, dan penjualan makanan dan minuman. Ini mencakup berbagai jenis usaha yang terlibat dalam persiapan makanan, kreasi menu, penyajian estetis, dan pengalaman kuliner secara keseluruhan.Kategori bidang usaha di sektor kuliner yang berkaitan dengan pajak meliputi restoran, kafe, dan warung makan, di mana pengusaha wajib memahami dan mematuhi aturan perpajakan terkait pembayaran pajak pendapatan, pajak restauran, serta pelaporan transaksi dengan benar sesuai dengan regulasi yang berlaku.</p>
-                        <h3 class="privacy-list-title semi-bold mb-20"><span>2</span>.  Bidang Fashion </h3>
-                        <p class="privacy-desc margin-0">Bidang fashion merujuk pada industri yang terkait dengan desain, produksi, dan distribusi pakaian, aksesori, dan produk tekstil lainnya. Ini mencakup semua aspek yang berkaitan dengan gaya busana, desain pakaian, tren mode, dan estetika visual dalam dunia pakaian dan penampilan.Dalam industri fashion, kategori bidang usaha seperti desain pakaian, produksi pakaian, dan penjualan produk fashion menghadapi tanggung jawab perpajakan yang meliputi pembayaran pajak penghasilan dari penjualan, pajak impor jika terlibat dalam impor bahan atau produk jadi, serta pemahaman terhadap pajak pertambahan nilai (PPN) yang berlaku dalam rantai distribusi produk fashion.</p><br>
-                        <h3 class="privacy-list-title semi-bold mb-20"><span>3</span>. Bidang Otomotif </h3>
-                        <p class="privacy-desc margin-0">Bidang otomotif merujuk pada industri yang terkait dengan perancangan, produksi, penjualan, perawatan, dan pemeliharaan kendaraan bermotor seperti mobil, sepeda motor, truk, dan sejenisnya. Ini mencakup segala hal yang berkaitan dengan kendaraan, termasuk desain, teknologi, manufaktur, dan layanan purna jual.Dalam industri otomotif, seperti jasa perbaikan kendaraan, penjualan suku cadang, dan showroom mobil, pengusaha harus memperhitungkan kewajiban perpajakan seperti pembayaran pajak penghasilan dari penjualan kendaraan atau suku cadang, penerapan pajak pertambahan nilai (PPN) dalam transaksi penjualan sesuai dengan daerah masing- masing , serta pemahaman tentang pajak daerah yang mungkin berlaku dalam bisnis otomotif di wilayah tertentu.</p><br>
-                        <h3 class="privacy-list-title semi-bold mb-20"><span>4</span>. Bidang Agribisnis </h3>
-                        <p class="privacy-desc margin-0">Bidang agribisnis merujuk pada sektor industri yang melibatkan produksi, pengolahan, distribusi, dan pemasaran produk pertanian, peternakan, perikanan, dan kehutanan. Agribisnis melibatkan seluruh rantai nilai dari produksi bahan baku pertanian hingga produk akhir yang siap dikonsumsi atau digunakan.Dalam agribisnis, seperti pertanian, perkebunan, dan peternakan, para pelaku usaha perlu memperhatikan kewajiban perpajakan seperti pembayaran pajak penghasilan dari hasil panen atau produksi, penerapan pajak pertambahan nilai (PPN) pada penjualan produk pertanian, serta pemenuhan aturan pajak terkait dengan impor atau ekspor produk pertanian jika terlibat dalam perdagangan internasional.</p><br>
-                        <h3 class="privacy-list-title semi-bold mb-20"><span>5</span>.   Bidang Komestik </h3>
-                        <p class="privacy-desc margin-0">Bidang kosmetik merujuk pada industri yang terkait dengan produk-produk yang digunakan untuk perawatan tubuh dan penampilan, seperti produk kecantikan, perawatan kulit, riasan, parfum, dan produk perawatan rambut. .Dalam menjalankan bisnis kosmetik, ada beberapa macam kategori usaha produk kosmetik yang dijalankan, yakni apakah sebagai produsen kosmetik, distributor, atau menjual barang kosmetik impor. Hal itu juga dapat memengaruhi jenis pajak yang dikenakan dan menjadi tanggung jawabnya.Bukan hanya pengusaha kosmetik yang kegiatannya menjual barang dengan cara mengimpor produk kosmetik siap jual, namun produsen kosmetik biasanya juga melakukan kegiatan impor bahan baku kosmetik untuk produksinya.</p><br>
-                        <h3 class="privacy-list-title semi-bold mb-20"><span>6</span>.   Bidang Event Organizer </h3>
-                        <p class="privacy-desc margin-0">Bidang Event Organizer (EO) merujuk pada industri yang berkaitan dengan perencanaan, pengorganisasian, dan pelaksanaan berbagai jenis acara atau kegiatan.Dalam industri event organizer, perusahaan atau individu yang menyelenggarakan acara-acara, termasuk pameran, konser, dan konferensi, perlu memperhitungkan aspek perpajakan seperti pembayaran pajak penghasilan dari pendapatan yang diperoleh dari penyelenggaraan acara, penerapan pajak pertambahan nilai (PPN) pada penjualan tiket atau layanan terkait acara, serta pemenuhan kewajiban pelaporan dan pembayaran pajak sesuai dengan regulasi yang berlaku.</p><br>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/fashion.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bidang Fashion</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/otomotif.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bidang Otomotif</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/agri.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bidang Agribisnis</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/kosmetik.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bidang Komestik</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/img/event-organizer.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bidang Event Organizer</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Privacy Policy End -->
-        <?php include './layout/footer.php'; ?>
+    </div>
+    <!-- Privacy Policy End -->
+    <?php include './layout/footer.php'; ?>
 </body>
+
 </html>
